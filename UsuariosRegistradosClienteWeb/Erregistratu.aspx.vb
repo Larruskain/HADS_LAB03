a66226dc9bf9176ae30e1fc31597390eb9ef4224
Joanes1 <98975037+Joanes1@users.noreply.github.com>
2023-02-23 15:43:00 +0100

    login

--- UsuariosRegistradosClienteWeb/Erregistratu.aspx.vb
+++ UsuariosRegistradosClienteWeb/Erregistratu.aspx.vb
@@ -11,23 +11,27 @@ Public Class Erregistratu
 
         Dim kont As Integer = 0
         Dim egiaztatzeZenbakia As Integer = CLng(Rnd() * 9000000) + 1000000
-        Dim PErabiltzaileMota As String = "normalUser"
+
         Dim a1 As String = TextBox1.Text
         Dim a2 As String = TextBox2.Text
         Dim a3 As String = TextBox3.Text
         Dim a4 As String = TextBox4.Text
         Dim a5 As String = TextBox5.Text
-        Dim a6 As Integer = TextBox6.Text
+        Dim a6 As String = TextBox6.Text
         Dim a9 As String = TextBox9.Text
         Dim a10 As String = TextBox10.Text
         Dim a11 As String = TextBox11.Text
 
         Dim emailCorrecto As Boolean = Ce.EsCorrectoForm(a1)
 
-        If a1 = "" Or a2 = "" Or a3 = "" Or a4 = "" Or a5 = "" Or a9 = "" Or a10 = "" Or a11 = "" Then
+        If a1 = "" Or a2 = "" Or a3 = "" Or a4 = "" Or a5 = "" Or a6 = Nothing Or a9 = "" Or a10 = "" Or a11 = "" Then
             kont = kont + 1
             Label5.Text = "Tienes que rellenar todos los datos para poder registrarte"
 
+        End If
+        If Not IsNumeric(a6) Then
+            kont = kont + 1
+            Label6.Text = "El DNI hay introducirlo sin la letra"
         End If
 
         If Not emailCorrecto Then
@@ -39,24 +43,32 @@ Public Class Erregistratu
         If (kont = 0) Then
             Try
                 'konprobau email hori dauken usuaxoik eztaola datubasin
+
                 If Not Ad.ObtenerUsuario(a1).HasRows Then
                     Ad.CerrarConexion()
                     Ad.Conectar()
-                    Dim InsUser As Integer = Ad.InsertarUsuario(a1, a2, a3, a4, a5, a6, egiaztatzeZenbakia, 0, a9, a10, PErabiltzaileMota, a11)
-
+                    Dim InsUser As Integer = Ad.InsertarUsuario(a1, a2, a3, a4, a5, CInt(a6), egiaztatzeZenbakia, 0, a9, a10, "normalUser", a11)
+                    Session("email") = a1
+                    'Dim InsUser2 As Integer = Ad.InsertarUsuario2(a1, a2)
                     Label1.Text = " "
                     Label3.Text = " "
                     Label4.Text = " "
                     Label5.Text = " "
-                    Label2.Text = "Te has registrado correctamente. Haz click en el siguiente enlace para hacer la verificación"
+                    Label6.Text = " "
+                    Label2.Text = "Te has registrado correctamente. Haz click en el siguiente enlace para completar la verificación"
                     'hyperlink-e 
-
+                    Dim pageid As String = "Egiaztatu.aspx"
+                    Dim port As Integer = Request.ServerVariables("server_port")
+                    HyperLink1.Visible = True
+                    HyperLink1.NavigateUrl = String.Format("https://localhost:" & port & "/Egiaztatu.aspx?erab=" & a1 & "&egZenb=" & egiaztatzeZenbakia, pageid)
+                    HyperLink1.Text = String.Format("http://localhost:" & port & "/Egiaztatu.aspx?erab=" & a1 & "&egZenb=" & egiaztatzeZenbakia)
 
                 Else
                     'error, 
                     Label3.Text = " "
                     Label4.Text = " "
                     Label5.Text = " "
+                    Label6.Text = " "
                     Label1.Text = "En la base de datos ya se encuentra un usuario con la misma dirección de email"
                     Label2.Text = " "
                 End If
@@ -66,6 +78,7 @@ Public Class Erregistratu
 
             End Try
         End If
+
 
 
         Ad.CerrarConexion()
