b6f0ae5853e6caf49945db36237e7463bed3a9f5
Joanes1 <98975037+Joanes1@users.noreply.github.com>
2023-03-02 23:01:23 +0100

    m

--- UsuariosRegistradosClienteWeb/TareaGenericasAlumnos.aspx.vb
+++ UsuariosRegistradosClienteWeb/TareaGenericasAlumnos.aspx.vb
@@ -3,50 +3,13 @@
 Public Class TareaGenericasAlumnos
     Inherits System.Web.UI.Page
 
-
-    'connection_DB_HADS.Open()
-
-
-
-
-
-    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
-        Dim dSet As New DataSet
-        Dim tblMbrs As New DataTable
-        Dim connection_DB_HADS = New SqlConnection("Server=tcp:hads1.database.windows.net,1433;Initial Catalog=SGTA2023;Persist Security Info=False;User ID=elarruscain003@ikasle.ehu.eus@hads1;Password=enekojoanes_HADS;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
-        Dim command_DB_HADS As SqlCommand
-
-
-        If Page.IsPostBack Then
-            dSet = Session("datos")
-
-        Else
-            connection_DB_HADS.Open()
-            Dim strSQL = "SELECT TG.codigo,TG.descripcion,TG.hEstimadas,TG.tipoTarea FROM ((EstudianteGrupo As EG INNER JOIN GrupoClase AS GC ON  EG.grupo = GC.codigo) INNER JOIN Asignatura AS A ON GC.codigoAsig=A.codigo) INNER JOIN TareaGenerica AS TG ON TG.codAsig = A.codigo WHERE (EG.email='" + Session("email") + "')"
-            Dim adap = New SqlDataAdapter(strSQL, connection_DB_HADS)
-            adap.Fill(dSet)
-
-            Session("datos") = dSet
-
-            tblMbrs = dSet.Tables(0)
-            Dim dView = New DataView(tblMbrs)
-            dView.RowFilter = "codigo LIKE '%" & DropDownList1.SelectedValue & "%'"
-            GridView1.DataSource = dView
-
-            GridView1.DataBind()
-
-            connection_DB_HADS.Close()
-        End If
-
-
-    End Sub
-
-    Protected Sub DropDownList1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList1.SelectedIndexChanged
-        Dim dSet2 = Session("datos")
-        Dim tblMbrs2 = dSet2.Tables(0)
-        Dim dView = New DataView(tblMbrs2)
-        dView.RowFilter = "codigo LIKE '%" & DropDownList1.SelectedValue & "%'"
-        GridView1.DataSource = dView
-        GridView1.DataBind()
+    Protected Sub GridView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles GridView1.SelectedIndexChanged
+        Dim indizea As Integer
+        indizea = GridView1.SelectedIndex
+        Session("cod") = GridView1.Rows(indizea).Cells(1).Text
+        Session("descr") = GridView1.Rows(indizea).Cells(2).Text
+        Session("hEst") = GridView1.Rows(indizea).Cells(3).Text
+        Session("tipoTarea") = GridView1.Rows(indizea).Cells(4).Text
+        Response.Redirect("TareasPropiasAlumnos.aspx")
     End Sub
 End Class
--- UsuariosRegistradosClienteWeb/TareasPropiasAlumnos.aspx.vb
+++ UsuariosRegistradosClienteWeb/TareasPropiasAlumnos.aspx.vb
@@ -1,9 +1,36 @@
-﻿Public Class TareasPropiasAlumnos
+﻿Imports System.Data.SqlClient
+
+Public Class TareasPropiasAlumnos
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         TextBox1.Text = Session("email")
+        TextBox2.Text = Session("cod")
+        TextBox3.Text = Session("hEst")
+        Session("hReales") = TextBox4.Text
     End Sub
 
+    Protected Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Response.Redirect("TareasGenericasAlumnos.aspx")
+    End Sub
 
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim conClsf As SqlConnection
+        conClsf = New SqlConnection("Server=tcp:hads1.database.windows.net,1433;Initial Catalog=SGTA2023;Persist Security Info=False;User ID=elarruscain003@ikasle.ehu.eus@hads1;Password=enekojoanes_HADS;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
+        Dim dapMbrs As New SqlDataAdapter()
+        Dim dstMbrs As New DataSet
+        Dim tblMbrs As New DataTable
+
+        Dim tblMbrs = dstMbrs.Tables("EstudianteTareas")
+        Dim row As DataRow = tblMbrs.NewRow()
+        row("email") = Session("email")
+        row("codTarea") = Session("cod")
+        row("hEstimadas") = Session("hEst")
+        row("hReales") = Session("hReales")
+        tblMbrs.Rows.Add(row)
+        GridView1.DataSource = tblMbrs
+        GridView1.DataBind()
+
+
+    End Sub
 End Class
