0b5b7fedeaac6cf6e07a79c0a18cccf68a4e51ad
Joanes1 <98975037+Joanes1@users.noreply.github.com>
2023-03-02 19:45:30 +0100

    a

--- UsuariosRegistradosClienteWeb/TareaGenericasAlumnos.aspx.vb
+++ UsuariosRegistradosClienteWeb/TareaGenericasAlumnos.aspx.vb
@@ -8,8 +8,8 @@ Public Class TareaGenericasAlumnos
     Dim strconnection_DB_HADS As String = "Server=tcp:hads1.database.windows.net,1433;Initial Catalog=SGTA2023;Persist Security Info=False;User ID=elarruscain003@ikasle.ehu.eus@hads1;Password=enekojoanes_HADS;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 
     'connection_DB_HADS.Open()
-    Dim dAdap As New SqlDataAdapter
-    Dim strSQL As String
+
+
     Dim dSet As New DataSet
     Dim tblMbrs As New DataTable
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
@@ -18,12 +18,10 @@ Public Class TareaGenericasAlumnos
             dSet = Session("datos")
 
         Else
-            connection_DB_HADS = New SqlConnection(strconnection_DB_HADS)
-            strSQL = "SELECT TG.codigo,TG.descripcion,TG.hEstimadas,TG.tipoTarea FROM ((EstudianteGrupo As EG INNER JOIN GrupoClase AS GC ON  EG.grupo = GC.codigo) INNER JOIN Asignatura AS A ON GC.codigoAsig=A.codigo) INNER JOIN TareaGenerica AS TG ON TG.codAsig = A.codigo WHERE (EG.email= +'" + Session("email") + "')AND (TG.explotacion =" + 1 + ")"
-            command_DB_HADS = New SqlCommand(strSQL)
-            command_DB_HADS.Connection = connection_DB_HADS
-            dAdap.SelectCommand = command_DB_HADS
-            dAdap.Fill(dSet, "Tareas")
+
+            Dim dAdap As New System.Data.SqlClient.SqlDataAdapter
+            dAdap = UsuariosRegistrados.AccesoDatos.ObtenerTareasAsignAlumno(Session("email"))
+            dAdap.Fill(dSet)
             tblMbrs = dSet.Tables("Tareas")
             GridView1.DataSource = tblMbrs
             GridView1.DataBind()
