b86076670c6b2002931ab0e11da26d19ec723148
Joanes1 <98975037+Joanes1@users.noreply.github.com>
2023-03-02 21:09:17 +0100

    a

--- UsuariosRegistradosClienteWeb/TareaGenericasAlumnos.aspx.vb
+++ UsuariosRegistradosClienteWeb/TareaGenericasAlumnos.aspx.vb
@@ -11,28 +11,29 @@ Public Class TareaGenericasAlumnos
 
 
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
-        Dim tblMbrs As New DataTable
         Dim dSet As New DataSet
-
-        Dim connection_DB_HADS As SqlConnection
+        Dim tblMbrs As New DataTable
+        Dim connection_DB_HADS = New SqlConnection("Server=tcp:hads1.database.windows.net,1433;Initial Catalog=SGTA2023;Persist Security Info=False;User ID=elarruscain003@ikasle.ehu.eus@hads1;Password=enekojoanes_HADS;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
         Dim command_DB_HADS As SqlCommand
-        Dim strconnection_DB_HADS As String = "Server=tcp:hads1.database.windows.net,1433;Initial Catalog=SGTA2023;Persist Security Info=False;User ID=elarruscain003@ikasle.ehu.eus@hads1;Password=enekojoanes_HADS;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+
 
         If Page.IsPostBack Then
             dSet = Session("datos")
 
         Else
-            Dim adap As New System.Data.SqlClient.SqlDataAdapter
-            connection_DB_HADS = New SqlConnection(strconnection_DB_HADS)
             connection_DB_HADS.Open()
             Dim strSQL = "SELECT TG.codigo,TG.descripcion,TG.hEstimadas,TG.tipoTarea FROM ((EstudianteGrupo As EG INNER JOIN GrupoClase AS GC ON  EG.grupo = GC.codigo) INNER JOIN Asignatura AS A ON GC.codigoAsig=A.codigo) INNER JOIN TareaGenerica AS TG ON TG.codAsig = A.codigo WHERE (EG.email='" + Session("email") + "')"
-            command_DB_HADS = New SqlCommand(strSQL)
-            command_DB_HADS.Connection = connection_DB_HADS
-            adap.SelectCommand = command_DB_HADS
+            Dim adap = New SqlDataAdapter(strSQL, connection_DB_HADS)
             adap.Fill(dSet)
-            Session("datos") = dSet
-            GridView1.DataSource = dSet
 
+            Session("datos") = dSet
+
+            tblMbrs = dSet.Tables(0)
+            Dim dView = New DataView(tblMbrs)
+            dView.RowFilter = "codigo LIKE '%" & DropDownList1.SelectedValue & "%'"
+            GridView1.DataSource = dView
+
+            GridView1.DataBind()
 
             connection_DB_HADS.Close()
         End If
@@ -42,8 +43,8 @@ Public Class TareaGenericasAlumnos
 
     Protected Sub DropDownList1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList1.SelectedIndexChanged
         Dim dSet2 = Session("datos")
-        Dim tblMbrs = dSet2.Tables(0)
-        Dim dView = New DataView(tblMbrs)
+        Dim tblMbrs2 = dSet2.Tables(0)
+        Dim dView = New DataView(tblMbrs2)
         dView.RowFilter = "codigo LIKE '%" & DropDownList1.SelectedValue & "%'"
         GridView1.DataSource = dView
         GridView1.DataBind()
